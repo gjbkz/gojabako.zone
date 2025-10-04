b5d45dfdbd76e3b2f39db3e5f6dfe65807bfc594
Kei Ito <kei.itof@gmail.com>
2025-10-04 14:41:14 +0900

    deps: @fontsource/noto-sans-jp:5.2.5→5.2.8 @mdx-js/loader:3.1.0→3.1.1 @next/mdx:15.3.0→15.5.4 @opentelemetry/otlp-transformer:0.200.0→0.205.0 @opentelemetry/sdk-logs:0.200.0→0.205.0 @playwright/test:1.52.0→1.55.1 @types/node:22.15.16→24.6.2 @types/react:19.1.3→19.2.0 acorn:8.14.1→8.15.0 lint-staged:15.5.0→16.2.3 next:15.2.4→15.5.4 postcss:8.5.3→8.5.6 sharp:0.34.1→0.34.4 typescript:5.8.3→5.9.3

--- src/util/otel/OtelLogExporter.ts
+++ src/util/otel/OtelLogExporter.ts
@@ -36,7 +36,9 @@ export class OtelLogExporter implements LogRecordExporter {
 		if (endpoint && commonHeaders) {
 			const promise = fetch(endpoint, {
 				method: "POST",
-				body: JsonLogsSerializer.serializeRequest(logs),
+				body: JsonLogsSerializer.serializeRequest(
+					logs,
+				) as ArrayBufferView<ArrayBuffer>,
 				headers: new Headers(commonHeaders),
 			})
 				.then((res) => handleResponse(resultCallback, res))
